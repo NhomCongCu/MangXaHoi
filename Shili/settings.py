01ee993d0ff5757dd4a1881bc9c57deae11a6e42
= <vansang10a6txqt@gmail.com>
2021-05-08 01:45:28 +0700

    msms

--- Shili/settings.py
+++ Shili/settings.py
@@ -121,21 +121,17 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 #
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# if DEBUG:
-#     STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'static')
-#     ]
-# else:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
+STATIC_URL = "/staticfiles/"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "staticfiles",
+#     '/staticfiles/',
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
