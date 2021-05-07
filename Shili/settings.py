731d0d37516a9a46e26e0f78ca23dffc6604c34c
= <vansang10a6txqt@gmail.com>
2021-05-08 01:36:45 +0700

    msms

--- Shili/settings.py
+++ Shili/settings.py
@@ -121,12 +121,14 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 #
-STATIC_URL = "/staticfiles/"
-# STATICFILES_DIRS = [
-#     BASE_DIR / "staticfiles",
-#     '/staticfiles/',
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
+STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
