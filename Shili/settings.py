e797f02c687d13b523560807c6d485c07ee9b79d
= <vansang10a6txqt@gmail.com>
2021-05-07 14:15:41 +0700

    first commit for all files

--- Shili/settings.py
+++ Shili/settings.py
@@ -121,11 +121,11 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 #
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/static/',
-]
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/static/',
+# ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = "/static/"
 #
